c59448829655c34f749425bf73f8b73b9c2d3581
firestorm0986 <firestorm0986@gmail.com>
2022-09-29 11:34:05 -0700

    aditya

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def index():
 def stub():
     return render_template("stub.html")
 
+@app.route('/programpurpose/')  # connects /stub/ URL to stub() function
+def programpurpose():
+    return render_template("programpurpose.html")
+
 # this runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
